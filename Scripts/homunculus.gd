cd99e3a348f250dc74d5ff25761c4061dfde60bb
Matt-Lamo <mattlejeune.lamogau@gmail.com>
2025-06-10 15:34:47 -0400

    movement/ animation updated
    hazards tilesheet added
    tutorial.tscn created
    homunculus.tscn movement and animations updated

--- Scripts/homunculus.gd
+++ Scripts/homunculus.gd
@@ -3,12 +3,13 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -17,6 +18,12 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction !=0:   
+		animated_sprite_2d.flip_h = (direction ==-1)  #flips animation 
+		animated_sprite_2d.play("walking")
+	else:
+		animated_sprite_2d.play("idle")
+		
 	if direction:
 		velocity.x = direction * SPEED
 	else:
