e6c16e08602000a4443872c8f78a5134aa301340
Matt-Lamo <mattlejeune.lamogau@gmail.com>
2025-09-16 12:41:47 -0400

    Game Over Screen Added

--- Scripts/homunculus.gd
+++ Scripts/homunculus.gd
@@ -4,6 +4,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const sawDamage = 15
 const hazardDamage = 15
+const wallGravity = 0
 var deathBool = true #flips when player dies to break physics_process loop
 enum States {IDLE, WALKING, FALLING, WALL_SLIDING, JUMPING}
 var state: States = States.IDLE
@@ -20,22 +21,25 @@ func check_charge() -> void:
 			deathBool = false
 			#animatedSprite2d.play("dead") NEED TO DRAW DEATH ANIMATION
 			set_physics_process(false)
-
-
-func check_state() -> void: #checks state and updates behavior
-	pass
+			
 		
 
 func set_state(direction: float) -> void:
-	if not is_on_floor():
-		state = States.FALLING
-		print("state FALLING  ")
 	if direction !=0:   
 		state = States.WALKING
 		print("state WALKING")
+	elif direction !=0 and Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		state = States.JUMPING
+		print("state JUMPING")
 	else:
 		state = States.IDLE
 		print("state IDLE")
+	if not is_on_floor():
+		state = States.FALLING
+		print("state FALLING  ")
+	#if is_on_wall_only():
+		#state = States.WALL_SLIDING
+		#print("state WALL SLIDING")
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		state = States.JUMPING
 		print("state JUMPING")
@@ -62,6 +66,9 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY #JUMPING STATE
 	elif state == States.FALLING:
 		pass#velocity += get_gravity() * delta  #FALLING STATE
+	elif state == States.WALL_SLIDING:
+		pass
+	
 	
 	move_and_slide()
 
